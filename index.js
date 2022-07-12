6c93a2f0099e069d36b09e8e076411d92e00ca76
Effiong Israel Robert <israelrobert@gmail.com>
2022-07-12 21:10:02 +0100

    created function to increment and decrement

---
+++ index.js
@@ -0,0 +1,10 @@
+let countNumber = document.getElementById("count-number")
+let count = 0
+function button(){
+    count = count + 1
+    countNumber.innerText = count
+}
+function buttonMinus(){
+    count = count - 1
+    countNumber.innerText = count
+}
