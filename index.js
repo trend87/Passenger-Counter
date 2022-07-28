42d4e8390bdff3e7dc4d7bcad069eb29ec3ef8c8
Effiong Israel Robert <israelrobert@gmail.com>
2022-07-28 21:25:33 +0100

    finished first and second section of learning how to create a passenger counter app

--- index.js
+++ index.js
@@ -1,14 +1,95 @@
-let countNumber = document.getElementById("count-number")
-let count = 0
-function button(){
-    count = count + 1
-    countNumber.innerText = count
-}
-function buttonMinus(){
-    count = count - 1
-    countNumber.innerText = count
-}
 
-function save(){
-    console.log(count)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FINISHED FIRST AND SECOND SECTION OF LEARNING TO CREATE A PASSENGER COUNTER APP
+
+// let num1 = 8
+// let num2 = 2
+// document.getElementById("num1-el").textContent = num1
+// document.getElementById("num2-el").textContent = num2
+
+// function add(){
+//     let sumResult = num1 + num2
+//     document.getElementById("sum-el").textContent = "sum " + sumResult
+// }
+
+// function sub(){
+//     let sumResult = num1 - num2
+//     document.getElementById("sum-el").textContent  = "sum " + sumResult
+// }
+
+// function divide(){
+//     let sumResult = num1 / num2
+//     document.getElementById("sum-el").textContent  = "sum " + sumResult
+// }
+
+// function multiply(){
+//     let sumResult = num1 * num2
+//     document.getElementById("sum-el").textContent  = "sum " + sumResult
+// }
+
+
+
+// let errorEl = document.getElementById("error")
+
+// function error(){
+//     errorEl.innerText = "Something went wrong, please try again"
+// }
+
+
+// saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-number")
+// let count = 0
+
+// function button(){
+//     count += 1
+//     countEl.innerText = count
+// }
+
+// function save(){
+//     let busEntries = " " + count + " - "
+//     saveEl.innerText += busEntries
+//     countEl.innerText = 0
+//     count = 0
+//     console.log(count)
+// }
+
+// let welcomeEl = document.getElementById("welcome")
+// let name = "Effiong Israel Robert"
+// let greeting = "Hi  Welcome back to the course "
+
+// welcomeEl.innerText = greeting + name 
+
+
+// welcomeEl.innerText += " emoji"
+
+
+
+
+// let countNumber = document.getElementById("count-number")
+// let count = 0
+// function button(){
+//     count += 1
+//     countNumber.innerText = count
+// }
+// function buttonMinus(){
+//     count -= 1
+//     countNumber.innerText = count
+// }
+
+// function save(){
+//     console.log(count)
+// }
