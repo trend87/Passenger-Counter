8aae444c015a943c97d1faf7df572d71864ab87c
Effiong Israel Robert <israelrobert@gmail.com>
2022-08-07 21:49:27 +0100

    displayed sum

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ let sum = firstCard + secondCard
 let hasBlackJack = false 
 let isAlive = true
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
 function startGame(){
+    sumEl.textContent += " " + sum
     if (sum <= 20){
         message = ("Do you want to draw another card?")
         
